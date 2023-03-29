735b5b0bdead60a56b040f83ea22235af7031801
Rarstyak <rarstyak@gmail.com>
2023-03-29 17:02:26 -0400

    Add contact cards

--- src/tab-contact.js
+++ src/tab-contact.js
@@ -7,44 +7,97 @@ function makeContact(name, title, number, mail, img) {
     div.classList.add('card-contact');
 
     const nameE = document.createElement('h4');
+    nameE.classList.add('card-contact-name');
     nameE.textContent = name;
     div.appendChild(nameE);
 
-    const titleE = document.createElement('p');
+    const titleE = document.createElement('div');
+    titleE.classList.add('card-contact-title');
     titleE.textContent = title;
     div.appendChild(titleE);
 
+    const text = document.createElement('div');
+    text.classList.add('card-contact-text');
+
     const numberE = document.createElement('a');
+    numberE.classList.add('card-contact-number');
     numberE.href = `tel:${number}`;
     numberE.textContent = number;
-    div.appendChild(numberE);
 
     const mailE = document.createElement('a');
+    mailE.classList.add('card-contact-mail');
     mailE.href = `mailto:${mail}`;
     mailE.textContent = mail;
-    div.appendChild(mailE);
+
+    text.innerHTML = `Contact Info:`;
+    text.innerHTML += `<br><br>`;
+    text.appendChild(numberE);
+    text.innerHTML += `<br><br>`;
+    text.appendChild(mailE);
+    div.appendChild(text);
 
     const imgE = document.createElement('img');
+    imgE.classList.add('card-contact-img');
     imgE.src = img;
     div.appendChild(imgE);
 
     return div;
 }
 
-export default function build() {
-    const div = document.createElement('div');
+function makeStaff() {
+    const cat = document.createElement('div');
+    cat.classList.add(`category`);
 
-    const h2 = document.createElement('h2');
-    h2.textContent = 'Contact';
-    div.appendChild(h2);
+    const h3 = document.createElement('h3');
+    h3.textContent = 'Staff';
+    cat.appendChild(h3);
 
     const container = document.createElement('div');
     container.classList.add('card-contact-container');
-    div.appendChild(container);
+    cat.appendChild(container);
 
     container.appendChild(makeContact('Elsje', 'Owner, Chef', '(123)456-7890', 'mail@real.com', Elsje));
     container.appendChild(makeContact('Fuuka', 'Server', '(123)456-7890', 'mail@real.com', Fuuka));
     container.appendChild(makeContact('Priscilla', 'Sister', '(123)456-7890', 'mail@real.com', Priscilla));
 
+    return cat;
+}
+
+function makeHours() {
+    const cat = document.createElement('div');
+    cat.classList.add(`category`);
+
+    const h3 = document.createElement('h3');
+    h3.textContent = 'Hours';
+    cat.appendChild(h3);
+
+    const container = document.createElement('div');
+    container.classList.add('card-hours-container');
+    cat.appendChild(container);
+
+    const hours = document.createElement('div');
+    hours.innerHTML = `Weekdays: 12pm-9pm`;
+    hours.innerHTML += `<br><br>`;
+    hours.innerHTML += `Holidays: 12pm-9pm`;
+
+    container.appendChild(hours);
+
+    return cat;
+}
+
+export default function build() {
+    const div = document.createElement('div');
+
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Contact Us';
+    div.appendChild(h2);
+
+    const subMenu = document.createElement('div');
+    subMenu.classList.add('category-container');
+    div.appendChild(subMenu);
+
+    subMenu.appendChild(makeHours());
+    subMenu.appendChild(makeStaff());
+    
     return div;
 }
--- src/tab-menu.js
+++ src/tab-menu.js
@@ -1,54 +1,12 @@
-// import Onigiri from './img/food/Onigiri.png';
-// import PickledTurnip from './img/food/Pickled Turnip.png';
-// import PikeSashimi from './img/food/Pike Sashimi.png';
-// import MasuTroutSashimi from './img/food/Masu Trout Sashimi.png';
-// import SaltedPondSmelt from './img/food/Salted Pond Smelt.png';
-// import SaltedPike from './img/food/Salted Pike.png';
-// import OrangeJuice from './img/food/Orange Juice.png';
-// import GrapeJuice from './img/food/Grape Juice.png';
-// import Bread from './img/food/Bread.png';
-// import BakedOnigiri from './img/food/Baked Onigiri.png';
-// import Dumplings from './img/food/Dumplings.png';
-// import Manju from './img/food/Manju.png';
-// import RicePorridge from './img/food/Rice Porridge.png';
-// import Marmalade from './img/food/Marmalade.png';
-
 import DataBank from './recipe_data.js';
 
 // Images are 128x128
 
-// function makeItem(name, desc, cost, img) {
-//     const div = document.createElement('div');
-//     div.classList.add('card-item');
-
-//     const nameE = document.createElement('h4');
-//     nameE.classList.add('card-item-name');
-//     nameE.textContent = name;
-//     div.appendChild(nameE);
-
-//     const descE = document.createElement('p');
-//     descE.classList.add('card-item-desc');
-//     descE.textContent = desc;
-//     div.appendChild(descE);
-
-//     const costE = document.createElement('div');
-//     costE.classList.add('card-item-cost');
-//     costE.textContent = cost;
-//     div.appendChild(costE);
-
-//     const imgE = document.createElement('img');
-//     imgE.classList.add('card-item-img');
-//     imgE.src = img;
-//     div.appendChild(imgE);
-
-//     return div;
-// }
-
 function accessItem(name, cost, desc) {
-    console.log(name.toUpperCase());
-    console.log(DataBank[0].name.toUpperCase())
+    // console.log(name.toUpperCase());
+    // console.log(DataBank[0].name.toUpperCase())
     const obj = DataBank.find( (item) => item.name.toUpperCase()==name.toUpperCase() );
-    console.log(obj);
+    // console.log(obj);
     
     const div = document.createElement('div');
     div.classList.add('card-item');
@@ -79,38 +37,6 @@ function accessItem(name, cost, desc) {
 
 export default function build() {
     const div = document.createElement('div');
-    // const menu = [
-    //     // Category name, array of items : name, desc, cost, img
-    //     ['Handmade', [
-    //         ['Onigiri', 'desc', '57 G', DataBank[0].img],
-    //         ['Onigiri', 'desc', '57 G', Onigiri],
-    //         ['Pickled Turnip', 'desc', '48 G', PickledTurnip],
-    //     ]],
-    //     ['Knife', [
-    //         ['Pike Sashimi', 'desc', '13 G', PikeSashimi],
-    //         ['Masu Trout Sashimi', 'desc', '17 G', MasuTroutSashimi],
-    //     ]],
-    //     ['Frying Pan', [
-    //         ['Salted Pond Smelt', 'desc', '173 G', SaltedPondSmelt],
-    //         ['Salted Pike', 'desc', '190 G', SaltedPike],
-    //     ]],
-    //     ['Mixer', [
-    //         ['Orange Juice', 'desc', '92 G', OrangeJuice],
-    //         ['Grape Juice', 'desc', '140 G', GrapeJuice],
-    //     ]],
-    //     ['Oven', [
-    //         ['Bread', 'desc', '145 G', Bread],
-    //         ['Baked Onigiri', 'desc', '65 G', BakedOnigiri],
-    //     ]],
-    //     ['Steamer', [
-    //         ['Dumplings', 'desc', '45 G', Dumplings],
-    //         ['Manju', 'desc', '272 G', Manju],
-    //     ]],
-    //     ['Pot', [
-    //         ['Rice Porridge', 'desc', '57 G', RicePorridge],
-    //         ['Marmalade', 'desc', '90 G', Marmalade],
-    //     ]],
-    // ]
 
     // via access
     const menu = [
@@ -166,7 +92,6 @@ export default function build() {
         cat.appendChild(container);
 
         category[1].forEach(item => {
-            // container.appendChild(makeItem(...item));
             // Use database instead of creating each
             container.appendChild(accessItem(...item));
         });
